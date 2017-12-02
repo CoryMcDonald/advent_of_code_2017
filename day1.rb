642b6390874d7cbd35ea757073186fe251912c53
Cory McDonald <cory.mcdonald@cerner.com>
2017-12-02 14:43:34 -0600

    Finish day1

--- day1.rb
+++ day1.rb
@@ -9,6 +9,16 @@ def solve(number)
   result
 end
 
-puts solve(1122)
-puts solve(1111)
-puts solve(1234)
+def part2(number)
+  result = 0
+  list = number.chars
+  length = list.length
+  edge = length / 2
+  list[0, edge].each_with_index do |x, index|
+    result += x.to_i if x == list[edge, length][index]
+  end
+
+  result*2
+end
+
+puts part2('123123')
